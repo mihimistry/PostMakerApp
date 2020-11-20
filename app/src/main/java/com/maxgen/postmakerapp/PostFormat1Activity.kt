3203b391a37a455929dd0dac43ab49c0d0d70976
mihimistry <mihimistry12@gmail.com>
2020-11-20 17:37:23 +0530

    Initial commit

---
+++ app/src/main/java/com/maxgen/postmakerapp/PostFormat1Activity.kt
@@ -0,0 +1,11 @@
+package com.maxgen.postmakerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostFormat1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_format1)
+    }
+}
